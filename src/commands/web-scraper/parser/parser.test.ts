74d8a18ee10f565d834da834affddff7ae9dd283
soleuniverse <dev@soleuniverse.me>
2025-08-28 02:24:41 +0200

    add full example parsing test

--- src/commands/web-scraper/parser/parser.test.ts
+++ src/commands/web-scraper/parser/parser.test.ts
@@ -23,5 +23,55 @@ describe("Parser", () => {
         },
       ]),
     );
+
+    expect(
+      Parser.parse(
+        parseTestBlock(`https://soleuniverse.me
+  h1
+  +, ul
+    @inline, li.class`),
+      ),
+    ).toMatchObject(
+      ok([
+        {
+          content: "https://soleuniverse.me",
+          astNode: {
+            type: "root",
+            url: new URL("https://soleuniverse.me"),
+          },
+          children: [
+            {
+              content: "h1",
+              astNode: {
+                type: "block",
+                selector: "h1",
+                modes: [],
+              },
+              children: [],
+            },
+            {
+              content: "+, ul",
+              astNode: {
+                type: "block",
+                selector: "ul",
+                modes: [],
+                quantifier: "+",
+              },
+              children: [
+                {
+                  content: "@inline, li.class",
+                  astNode: {
+                    type: "block",
+                    selector: "li.class",
+                    modes: ["inline"],
+                  },
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ]),
+    );
   });
 });
