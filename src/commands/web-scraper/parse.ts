4982efbf72984a274c1bb8ccb1fab9e3dd838aca
soleuniverse <dev@soleuniverse.me>
2025-09-04 04:34:05 +0200

    add: enable interpreter in logseq

--- src/commands/web-scraper/parse.ts
+++ src/commands/web-scraper/parse.ts
@@ -1,7 +1,29 @@
 import { BlockCommandCallback } from "@logseq/libs/dist/LSPlugin.user";
+import { Parser } from "./parser";
+import { contentsFromOutput, RawBlock } from "./parser/blocks";
+import { reportErrors } from "./errors/report";
+import { Interpreter } from "./interpreter";
 
 export default (async ({ uuid }) => {
   const rootSourceBlock = (await logseq.Editor.getBlock(uuid, {
     includeChildren: true,
   }))!;
+
+  const parsing = Parser.parse([rootSourceBlock as RawBlock]);
+  if (parsing.isErr()) {
+    return reportErrors(parsing);
+  }
+
+  const outputResult = await Interpreter.interpret(parsing.value);
+  if (outputResult.isErr()) {
+    return reportErrors(outputResult);
+  }
+
+  await logseq.Editor.insertBatchBlock(
+    uuid,
+    contentsFromOutput(outputResult.value),
+    {
+      sibling: true,
+    },
+  );
 }) satisfies BlockCommandCallback;
--- src/commands/web-scraper/parser/blocks.ts
+++ src/commands/web-scraper/parser/blocks.ts
@@ -1,5 +1,6 @@
 import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
 import { ASTNode } from "./ast";
+import { OutputNode } from "../interpreter";
 
 export type RawBlock = Pick<BlockEntity, "content" | "uuid"> & {
   children: RawBlock[];
@@ -13,3 +14,11 @@ export type ContentBlock = {
   content: string;
   children: ContentBlock[];
 };
+
+export function contentsFromOutput(outputNodes: OutputNode[]): ContentBlock[] {
+  const contents: ContentBlock[] = [];
+  for (const { content, children } of outputNodes) {
+    contents.push({ content, children: contentsFromOutput(children) });
+  }
+  return contents;
+}
